5113ed92ef6036f1e6ce66340eefab497d6ddb1a
Banwind <a84012807@gmail.com>
2023-03-12 13:55:44 +0800

    feat: find req.query && add crypto.js

--- routes/modules/short.js
+++ routes/modules/short.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const URLs = require('../../models/urls')
 
 router.get('/', (req, res) => {
+  const origin_url = req.query.origin_url // 獲取 URL 中的查詢參數（Query String），即 URL 中問號後面的參數
+  console.log(origin_url)
   URLs.find()
     .lean()
     .then(() => res.render('short.hbs'))
