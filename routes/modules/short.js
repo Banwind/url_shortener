a0c69f6e56ff0d585f8b3d3456a3ff9090bed3ec
Banwind <a84012807@gmail.com>
2023-03-12 15:05:36 +0800

    feat: store data function

--- routes/modules/short.js
+++ routes/modules/short.js
@@ -6,15 +6,24 @@ const short = require('../../public/js/crypto')
 router.get('/', (req, res) => {
   const origin_url = req.query.origin_url // 獲取 URL 中的查詢參數（Query String），即 URL 中問號後面的參數
   const short_url = short.hash(origin_url)
-  console.log(short_url)
-  URLs.find()
-    .lean()
-    .then(() => res.render('short.hbs', { short_url }))
-    .catch(error => console.error(error))
+  const URL = new URLs({ origin_url:origin_url, short_url:"http://localhost:3000/" + short_url})
+  
+  URLs.findOne({ origin_url })
+    .then((doc) => {
+      if (doc) {
+        console.log('URL already in the database')
+        res.render('short.hbs', { short_url })
+      } else {
+        URL.save()
+          .then(() => {
+            console.log('URL saved to database')
+            res.render('short.hbs', { short_url })
+          }) 
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
-
-
 
 // 還需要改動
